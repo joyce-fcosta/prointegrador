139967e380fe22288c7d091fabd4b9f27d94e5f7
joyce-fcosta <joyced.fcosta@gmail.com>
2020-10-21 15:43:55 -0300

    commit pagina dos cards

---
+++ paginas/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Card Expansion Effect - Demo 3</title>
+	<meta name="description" content="Card Expansion Effect with SVG clipPath" />
+	<meta name="keywords" content="clipPath, svg, effect, layout, expansion, images, grid, polygon" />
+	<meta name="author" content="Claudio Calautti for Codrops" />
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="public/css/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="public/css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.3.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="public/css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="public/css/card.css" />
+	<link rel="stylesheet" type="text/css" href="public/css/pattern.css" />
